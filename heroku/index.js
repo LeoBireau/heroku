ed213ca9e13e435fdd1ab155bc869322efc07cde
lemoine <lemoinelucas1@gmail.com>
2019-12-10 16:38:54 +0100

    sept

--- heroku/index.js
+++ heroku/index.js
@@ -5,37 +5,17 @@ const request = require("request");
 
 app.get('/', function (req, res) {
 
-	var options = { method: 'GET',
-	  url: 'https://dephero-b04e.restdb.io/rest/utilisateur',
-	  headers: 
-	   { 'cache-control': 'no-cache',
-	     'x-apikey': '4ee56fd28c586c7ce5e76b325264f34283e6a' } };
+	let config = {
+		'cache-control': 'no-cache',
+	    'x-apikey': '4ee56fd28c586c7ce5e76b325264f34283e6a',
+	    'content-type': 'application/json' 
+	};
 
-	request(options, function (error, response, body) {
-	  if (error) throw new Error(error);
+	axios
+      .get('https://dephero-b04e.restdb.io/rest/utilisateur', {headers: config})
+      .then(response => (res.send(response));
 
-	  res.send(body);
-	});
 })
-
-app.get('/', function (req, res) {
-	var options = { method: 'POST',
-	  url: 'https://dephero-b04e.restdb.io/rest/utilisateur',
-	  headers: 
-	   { 'cache-control': 'no-cache',
-	     'x-apikey': '4ee56fd28c586c7ce5e76b325264f34283e6a',
-	     'content-type': 'application/json' },
-	  body: { field1: 'xyz', field2: 'abc' },
-	  json: true };
-
-	request(options, function (error, response, body) {
-	  if (error) throw new Error(error);
-
-	  console.log(body);
-	});
-})
-
-
 
 app.listen(PORT, function () {
   console.log('Example app listening on port ' + PORT)
