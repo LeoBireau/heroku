b96a1f950edb13cde8c108d21c9ad642a41a2c54
lemoine <lemoinelucas1@gmail.com>
2019-12-10 15:15:25 +0100

    six

--- heroku/index.js
+++ heroku/index.js
@@ -1,17 +1,5 @@
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT || 5000 // this is very important
+
 const request = require("request");
-
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
-app.listen(PORT, function () {
-  console.log('Example app listening on port ' + PORT)
-})
-
-
 
 var options = { method: 'GET',
   url: 'https://dephero-b04e.restdb.io/rest/users',
