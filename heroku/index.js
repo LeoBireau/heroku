6b5bf34ebf828be8323e59540071e670b39c69f5
lemoine <lemoinelucas1@gmail.com>
2019-12-10 16:27:13 +0100

    sept

--- heroku/index.js
+++ heroku/index.js
@@ -16,9 +16,26 @@ app.get('/', function (req, res) {
 
 	  res.send(body);
 	});
-
-  
 })
+
+app.get('/', function (req, res) {
+	var options = { method: 'POST',
+	  url: 'https://dephero-b04e.restdb.io/rest/utilisateur',
+	  headers: 
+	   { 'cache-control': 'no-cache',
+	     'x-apikey': '4ee56fd28c586c7ce5e76b325264f34283e6a',
+	     'content-type': 'application/json' },
+	  body: { field1: 'xyz', field2: 'abc' },
+	  json: true };
+
+	request(options, function (error, response, body) {
+	  if (error) throw new Error(error);
+
+	  console.log(body);
+	});
+})
+
+
 
 app.listen(PORT, function () {
   console.log('Example app listening on port ' + PORT)
