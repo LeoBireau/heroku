94d88fcc3fe486a6ae94f8e2c28ba00f880c44fb
Krable <39793454+Krable@users.noreply.github.com>
2020-03-04 15:50:03 +0100

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -1,29 +1,36 @@
-const express = require('express')
-const request = require("request")
 const axios = require("axios")
+const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt')
+const express = require('express')
 const jwt = require('jsonwebtoken')
 const passport = require('passport')
 const passportJWT = require('passport-jwt')
-const bcrypt = require('bcrypt')
-const app = express()
-const PORT = process.env.PORT || 5000 // this is very important
-const bodyParser = require('body-parser')
+const secret = 'thisismysecret'
 const urlEncodedParser = bodyParser.urlencoded({ extended: false })
 
-const ExtractJwt = passportJWT.ExtractJwt
-const JwtStrategy = passportJWT.Strategy
-const jwtOptions = {
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: 'secret'
-}
+const app = express()
+
+const users = [{ email: 'pcavalet@kaliop.com', password: 'kaliop' }]
+
 const configuration = {
         'cache-control': 'no-cache',
         'x-apikey': '4ee56fd28c586c7ce5e76b325264f34283e6a',
         'content-type': 'application/json',
         'ObjectID' : '5df8db1291eb7c5a0001196b' 
 }
+
+const ExtractJwt = passportJWT.ExtractJwt
+const JwtStrategy = passportJWT.Strategy
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: secret
+}
+
 const jwtStrategy = new JwtStrategy(jwtOptions, function(payload, next) {
+  
   const user = users.find(user => user.email === payload.user)
+
   if (user) {
     next(null, user)
   } else {
@@ -33,47 +40,38 @@ const jwtStrategy = new JwtStrategy(jwtOptions, function(payload, next) {
 
 passport.use(jwtStrategy)
 
-app.get('/pw', function (req, res) {
-    bcrypt.genSalt(10, function(err, salt) {
-        bcrypt.hash("holla", salt, function(err, hash) {
-               bcrypt.compare("holla", hash, function(err, resp) {
-                   res.send(resp)
-            });
-        });
-    });
+app.get('/public', (req, res) => {
+  res.send('I am public folks!')
 })
 
-//Partie Utilisateur ***************************************************************************
-
-app.get('/getAllUsers', function (req, res) {
-    axios.get('https://dephero-b04e.restdb.io/rest/utilisateur', { headers: configuration } )
-      .then(response => (res.send(response.data))).catch(console.log)
+app.get('/private', passport.authenticate('jwt', { session: false }), (req, res) => {
+  res.send('Hello ' + req.user.email)
 })
 
-app.post("/addUser",urlEncodedParser, function (req, res) {
+app.post('/login', urlEncodedParser, (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+  console.log(req.body);
 
-    let salt = bcrypt.genSaltSync(10);
-    let hash = bcrypt.hashSync(req.body.password, salt);
+  if (!email || !password) {
+    console.log("pas de mail ou mdp")
+    res.status(401).json({ error: 'Email or password was not provided.' })
+    return
+  }
 
-    axios({
-        "method": "POST",
-        "url": "https://dephero-b04e.restdb.io/rest/utilisateur",
-        "headers": configuration,
-        "data": { 
-            "nomUser": req.body.nom,
-            "prenomUser": req.body.prenom,
-            "emailUser": req.body.email,
-            "passwordUser": hash
-        },
-          "responseType": 'json'
-    }).then(response => {
-        res.send(response.data);
-    }).catch(error => {
-        res.send(error);
-    })
+  // usually this would be a database call:
+  const user = users.find(user => user.email === email)
+
+  if (!user || user.password !== password) {
+    console.log("erreur mdp ou email")
+    res.status(401).json({ error: 'Email / password do not match.' })
+    return
+  }
+
+  const userJwt = jwt.sign({ user: user.email }, secret)
+
+  res.json({user, userJwt })
 })
-
-//Partie Articles *************************************************************************
 
 //Testé et Approuvé
 app.get('/getAllArticles', function (req, res) {
@@ -155,11 +153,6 @@ app.get("/createUser", function (req, res) {
     res.sendFile(__dirname+"/ressources/formCreateAccount.html");
 })
 
-
-
-//Partie écouteur
-
-app.listen(PORT, function () {
-  console.log('Example app listening on port ' + PORT)
+app.listen(5000, () => {
+  console.log('app running on port 5000')
 })
-
